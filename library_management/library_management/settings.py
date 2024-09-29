66d7b50d0c67357e121d0f2745144d750ab57575
oluwatimileyin <oluwatimileyin962@gmail.com>
2024-09-29 16:38:27 +0100

    changed debug to DEBUG

--- library_management/library_management/settings.py
+++ library_management/library_management/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("debug", "False").lower() in ("true", "1")
+DEBUG = os.environ.get("DEBUG", "False").lower() in ("true", "1")
 
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
