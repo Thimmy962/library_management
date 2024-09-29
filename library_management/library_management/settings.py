0b6316cdf7047271e26bf1e661eafea504b7c96a
oluwatimileyin <oluwatimileyin962@gmail.com>
2024-09-29 16:34:26 +0100

    DEBUG fix

--- library_management/library_management/settings.py
+++ library_management/library_management/settings.py
@@ -16,7 +16,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get("debug", "False").lower() in ("true", "1")
+
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
